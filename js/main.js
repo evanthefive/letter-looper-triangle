bbf3fd4b8681c46c2005360f5fb23f2b2ad5dd3f
Evan Lefebvre <lefe0177@algonquinlive.com>
2016-03-04 12:00:29 -0500

    Converted indentation to spaces

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var letter = prompt('Give me a letter');
 
 for (i = 0; i < 10; i++) {
 
-	for (k = i; k >= 0; k--) {
-		document.write(letter);
-	}
-	document.write('<br>');
+  for (k = i; k >= 0; k--) {
+    document.write(letter);
+  }
+  document.write('<br>');
 }
